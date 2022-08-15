c76d390cecbcbe58d4ff35e463d44f64f2557d4c
hassan-kodexo <hassan.raza@kodexolabs.com>
2022-08-15 13:35:08 +0500

    first commit

---
+++ components/Categories.js
@@ -0,0 +1,25 @@
+import { View, Text, ScrollView } from 'react-native'
+import React from 'react'
+import CategoryCard from './CategoryCard'
+
+const Categories = () => {
+  return (
+    <ScrollView
+    contentContainerStyle={{
+        paddingHorizontal: 15,
+        paddingTop: 10
+    }}
+    horizontal
+    showsHorizontalScrollIndicator={false}
+    >
+        {/* Categories Card*/}
+        <CategoryCard imgUrl="https://links.papareact.com/gn7" title="TESTING 1"/>
+        <CategoryCard imgUrl="https://links.papareact.com/gn7" title="TESTING 2"/>
+        <CategoryCard imgUrl="https://links.papareact.com/gn7" title="TESTING 3"/>
+        <CategoryCard imgUrl="https://links.papareact.com/gn7" title="TESTING 4"/>
+        <CategoryCard imgUrl="https://links.papareact.com/gn7" title="TESTING 5"/>
+    </ScrollView>
+  )
+}
+
+export default Categories 
---
+++ components/FeatureRow.js
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { ArrowRightIcon } from "react-native-heroicons/outline";
+
+const FeatureRow = ({ title, description, id }) => {
+  return (
+    <View>
+      <View className="mt-4 flex-row items-center justify-between px-4">
+        <Text className="font-bold text-lg">{title}</Text>
+        <ArrowRightIcon color="#00CCBB"/>
+      </View>
+      <Text>{description}</Text>
+    </View>
+  );
+};
+
+export default FeatureRow;
