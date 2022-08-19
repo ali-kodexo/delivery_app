1143c322818b6188087e5d697fb5a75946a45b03
ali-kodexo <ali.aftab@kodexolabs.com>
2022-08-19 19:45:23 +0500

    chore:restaurant screen

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -44,7 +44,7 @@ const RestaurantScreen = () => {
           className="w-full h-56 bg-gray-300 p-4"
         />
         <TouchableOpacity
-          className="absolute top-8 left-2 p-2 bg-gray-100 rounded-full"
+          className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full"
           onPress={() => navigation.goBack()}
         >
           <ArrowLeftIcon size={20} color="#00CCBB" />
@@ -63,34 +63,39 @@ const RestaurantScreen = () => {
 
             <View className="flex-row items-center space-x-1">
               <LocationMarkerIcon color="gray" opacity={0.4} size={22} />
-              <Text className="text-xs text-gray-500">Nearby . {address}</Text>
+              <Text className="text-xs text-gray-500">
+                Nearby . {address}
+              </Text>
             </View>
+
           </View>
           <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
         </View>
         <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
-          <QuestionMarkCircleIcon color="gray" opacity={0.6} size={20} />
-          <Text className="pl-2 flex-1 text-md font-bold">
-            Have a food allergy?
-          </Text>
-          <ChevronRightIcon color="#00CCBB" />
+            <QuestionMarkCircleIcon color="gray" opacity={0.6} size={20}/>
+            <Text className="pl-2 flex-1 text-md font-bold">
+                Have a food allergy?
+            </Text>
+            <ChevronRightIcon color="#00CCBB"/>
         </TouchableOpacity>
       </View>
       <View>
-        <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
+        <Text className="px-4 pt-6 mb-3 font-bold text-xl">
+            Menu
+        </Text>
         {dishes?.map((dish) => (
-          <DishRow
+            <DishRow
             key={dish._id}
             id={dish._id}
             name={dish.name}
             description={dish.short_description}
             price={dish.price}
             image={dish.image}
-          />
+            />
         ))}
       </View>
     </ScrollView>
   );
 };
-
+ 
 export default RestaurantScreen;
