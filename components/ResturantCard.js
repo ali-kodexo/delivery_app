533df78558d0a005f4b43331f537be192b3df144
talha-irshad-kodexo <talha.irshad@kodexolabs.com>
2022-08-19 09:57:44 +0500

    Detail Screen

--- components/ResturantCard.js
+++ components/ResturantCard.js
@@ -2,7 +2,8 @@ import { View, Text, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import { StarIcon } from "react-native-heroicons/solid";
 import { LocationMarkerIcon } from "react-native-heroicons/outline";
-import { urlFor } from '../sanity';
+import { urlFor } from "../sanity";
+import { useNavigation } from "@react-navigation/native";
 
 const ResturantCard = ({
   id,
@@ -16,8 +17,25 @@ const ResturantCard = ({
   long,
   lat,
 }) => {
+  const navigation = useNavigation();
   return (
-    <TouchableOpacity className='bg-white mr-3 shadow'>
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate("Restaurant", {
+          id,
+          imgUrl,
+          title,
+          rating,
+          genre,
+          address,
+          short_description,
+          dishes,
+          long,
+          lat,
+        });
+      }}
+      className="bg-white mr-3 shadow"
+    >
       <Image
         source={{
           uri: urlFor(imgUrl).url(),
@@ -33,9 +51,9 @@ const ResturantCard = ({
           </Text>
         </View>
 
-        <View className='flex-row items-center space-x-1'>
-            <LocationMarkerIcon color='gray' opacity={0.4} size={22}/>
-            <Text className='text-xs text-gray-500'>Nearby . {address}</Text>
+        <View className="flex-row items-center space-x-1">
+          <LocationMarkerIcon color="gray" opacity={0.4} size={22} />
+          <Text className="text-xs text-gray-500">Nearby . {address}</Text>
         </View>
       </View>
     </TouchableOpacity>
